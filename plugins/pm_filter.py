aca603d523ae3bc31db4ce5ebae2c3752abf2ad0
parvej6257 <aditikaur1017@gmail.com>
2025-05-26 09:46:26 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1957,7 +1957,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
     elif query.data == "about":
         buttons = [[
             InlineKeyboardButton('Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ', url="https://t.me/PKcontact_bot"),
-            InlineKeyboardButton('Sᴏᴜʀᴄᴇ Cᴏᴅᴇ', url="https://t.me/Parvej_117")
+            InlineKeyboardButton('🪔 Sᴏᴜʀᴄᴇ Cᴏᴅᴇ', callback_data='SOURCE_TXT')
         ],[
             InlineKeyboardButton('Hᴏᴍᴇ', callback_data='start'),
             InlineKeyboardButton('Cʟᴏsᴇ', callback_data='close_data')
